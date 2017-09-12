d078465f977b7d6d13eefbe926cf7c9724fbd28f
Connor Conway <connor_conway1@outlook.com>
2017-09-12 09:09:12 +0100

    Convert GenerateShape to experssion body

--- CodeWars/CodeWars/BuildASquare.cs
+++ CodeWars/CodeWars/BuildASquare.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 
 namespace CodeWars
 {
   //http://www.codewars.com/kata/build-a-square/train/csharp
   public static class BuildASquare
   {
-    public static string GenerateShape(int n)
-    {
-      return string.Join("\n", Enumerable.Repeat(new string('+', n), n));
-    }
+    public static string GenerateShape(int n) 
+      => string.Join("\n", Enumerable.Repeat(new string('+', n), n));
   }
 }
