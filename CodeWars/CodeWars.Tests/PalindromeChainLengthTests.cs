dbe761c08727c3b8dcd6659d4e6cfd78f35934ad
Connor Conway <connor_conway1@outlook.com>
2017-09-15 16:39:40 +0100

    PanlindroneChainLength Kata

---
+++ CodeWars/CodeWars.Tests/PalindromeChainLengthTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars.Tests
+{
+  [TestFixture]
+  public class PalindromeChainLengthTests
+  {
+    [TestCase(87, 4)]
+    [TestCase(10, 1)]
+    [TestCase(1, 0)]
+    [TestCase(88, 0)]
+    public void GivenAOutputShouldBeB(int n, int passes)
+    {
+      Assert.AreEqual(passes, PalindromeChainLength.palindromeChainLength(n));
+    }
+  }
+}
---
+++ CodeWars/CodeWars/PalindromeChainLength.cs
@@ -0,0 +1,35 @@
+﻿namespace CodeWars
+{
+  public static class PalindromeChainLength
+  {
+    //https://www.codewars.com/kata/palindrome-chain-length/train/csharp
+    public static int palindromeChainLength(int n)
+    {
+      var passes = 0;
+      var reverse = n.Reverse();
+      while (n != reverse)
+      {
+        n = n+reverse;
+        reverse = n.Reverse();
+        passes += 1;
+      }
+
+      return passes;
+    }
+
+
+    private static int Reverse(this int n)
+    {
+      var left = n;
+      var rev = 0;
+      while (left > 0)
+      {
+        var r = left % 10;
+        rev = rev * 10 + r;
+        left = left / 10;
+      }
+
+      return rev;
+    }
+  }
+}
