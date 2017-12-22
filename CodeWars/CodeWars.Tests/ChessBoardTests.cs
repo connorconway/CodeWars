1233ff1b287220a3c8b4fd7d834ad620f9e0ad93
Connor Conway <connor_conway1@outlook.com>
2017-12-22 12:14:20 +0000

    Chess Board Cell Color refactor

--- CodeWars/CodeWars.Tests/ChessBoardTests.cs
+++ CodeWars/CodeWars.Tests/ChessBoardTests.cs
@@ -10,15 +10,15 @@ namespace CodeWars.Tests
 			var kata = new ChessBoard();
 
 
-			Assert.AreEqual(true, kata.ChessBoardCellColor("A1", "A3"));
-			Assert.AreEqual(true, kata.ChessBoardCellColor("E2", "C4"));
-			Assert.AreEqual(true, kata.ChessBoardCellColor("B5", "B9"));
-			Assert.AreEqual(true, kata.ChessBoardCellColor("A5", "B8"));
-			Assert.AreEqual(true, kata.ChessBoardCellColor("B8", "A5"));
-			Assert.AreEqual(true, kata.ChessBoardCellColor("A1", "C3"));
-			Assert.AreEqual(false, kata.ChessBoardCellColor("B9", "A5"));
-			Assert.AreEqual(false, kata.ChessBoardCellColor("A1", "H3"));
-			Assert.AreEqual(false, kata.ChessBoardCellColor("A1", "A2"));
+			Assert.AreEqual(true, ChessBoard.ChessBoardCellColor("A1", "A3"));
+			Assert.AreEqual(true, ChessBoard.ChessBoardCellColor("E2", "C4"));
+			Assert.AreEqual(true, ChessBoard.ChessBoardCellColor("B5", "B9"));
+			Assert.AreEqual(true, ChessBoard.ChessBoardCellColor("A5", "B8"));
+			Assert.AreEqual(true, ChessBoard.ChessBoardCellColor("B8", "A5"));
+			Assert.AreEqual(true, ChessBoard.ChessBoardCellColor("A1", "C3"));
+			Assert.AreEqual(false, ChessBoard.ChessBoardCellColor("B9", "A5"));
+			Assert.AreEqual(false, ChessBoard.ChessBoardCellColor("A1", "H3"));
+			Assert.AreEqual(false, ChessBoard.ChessBoardCellColor("A1", "A2"));
 		}
 	}
 }
--- CodeWars/CodeWars/ChessBoard.cs
+++ CodeWars/CodeWars/ChessBoard.cs
@@ -9,24 +9,7 @@ namespace CodeWars
 	{
 		public static bool ChessBoardCellColor(string cell1, string cell2)
 		{
-			var oddList = new List<char> {'A', 'C', 'E', 'G'};
-			var evenList = new List<char> {'B', 'D', 'F', 'H'};
-
-			if (oddList.Contains(cell1[0]) && oddList.Contains(cell2[0]) || evenList.Contains(cell1[0]) && evenList.Contains(cell2[0]))
-			{
-				if (cell1[1] % 2 == 0 && cell2[1] % 2 == 0) return true;
-				return cell1[1] % 2 != 0 && cell2[1] % 2 != 0;
-			}
-
-			if (evenList.Contains(cell1[0]) && oddList.Contains(cell2[0]))
-			{
-				if (cell1[1] % 2 == 0 && cell2[1] % 2 != 0) return true;
-				return cell1[1] % 2 != 0 && cell2[1] % 2 == 0;
-			}
-
-			if (!oddList.Contains(cell1[0]) || !evenList.Contains(cell2[0])) return false;
-			if (cell1[1] % 2 == 0 && cell2[1] % 2 != 0) return true;
-			return cell1[1] % 2 != 0 && cell2[1] % 2 == 0;
+			return (cell1[0] + cell1[1]) % 2 == (cell2[0] + cell2[1]) % 2;
 		}
 	}
 }
